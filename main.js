2cf4f94ba6f530c747863a81568a0f6c2f3e8458
Ruth Moyano <ruth.moyano@codeimmersives.com>
2021-04-07 09:04:45 -0500

    {add changes to main.js file]

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
 // Setup 'tick' sound
 const tick = new Audio('sounds/tick.mp3');
+const tock = new Audio('sounds/tock.mp3');
+const kick = new Audio ('sounds/kick-drum.mp3')
+const snare = new Audio ('sounds/snare-drum.mp3')
+const hi = new Audio ('sounds/hi-hat.mp3')
+const count = document.querySelector("#timer")
 
+let timerCount = 0
+let displayCount = 1
 // This function is called every 600ms
 function update() {
-
+    const metronome = document.querySelector('#metronome')
+    const kickDrum = document.querySelector('#kick-drum')
+    const snareDrum = document.querySelector('#snare-drum')
+    const hiHat = document.querySelector('#hi-hat')
+    if (metronome.checked){
+        const count = document.querySelector('#timer')
+        count.innerText = num
+        if (timerCount%4===0){
+            tock.play()
+        } else{
+            tick.play();
+        }
+        timerCount++
+    } else if(kickDrum.checked){
+        kick.play()
+    } else if(snareDrum.checked){
+        snare.play()
+    } else if(hiHat.checked){
+        hi.play()
+    }
     // Play the 'tick' sound
     tick.play();
 }
